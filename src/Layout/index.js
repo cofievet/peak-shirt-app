0758ac17df311ba22f6321af037c620ac2d3966b
corentin.fievet <corentin.fievet@cdiscount.com>
2023-03-17 17:27:07 +0100

    feat: 🎸 add home link on app bar

--- src/Layout/index.js
+++ src/Layout/index.js
@@ -1,16 +1,18 @@
 import { ShoppingCart } from "@mui/icons-material";
 import { AppBar, Badge, IconButton, Toolbar, Typography } from "@mui/material";
 import { Box } from "@mui/system";
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 export default function Layout() {
   return (
     <>
       <AppBar>
         <Toolbar>
-          <Typography variant="h6" component="h1">
-            Peak Shirt
-          </Typography>
+          <Link to="/">
+            <Typography variant="h6" component="h1">
+              Peak Shirt
+            </Typography>
+          </Link>
           <Box sx={{ flexGrow: 1 }}></Box>
           <Box sx={{ display: { md: "flex" } }}>
             <IconButton
--- src/TshirtPage/index.js
+++ src/TshirtPage/index.js
@@ -1,3 +1,18 @@
+import TshirtCard from "TshirtCard";
+
 export default function TshirtPage() {
-  return <>TshirtPage</>;
+  const tshirt = {
+    id: 1,
+    name: "tshirt",
+    description: "test",
+    imageUrl: "1.jpg",
+    size: "S",
+    price: 12.99,
+  };
+
+  return (
+    <>
+      <TshirtCard {...tshirt} />
+    </>
+  );
 }
