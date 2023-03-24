58199365a7df0c1797cdf1a3938f6b87b674d435
corentin.fievet <corentin.fievet@cdiscount.com>
2023-03-24 11:33:39 +0100

    test: 💍 adapt TshirtPage tests

--- src/TshirtPage/index.test.js
+++ src/TshirtPage/index.test.js
@@ -1,12 +1,12 @@
 import render from "testUtils/render";
-import { screen } from "@testing-library/react";
+import { screen, waitFor } from "@testing-library/react";
 
 import TshirtPage from ".";
 import { CartProvider } from "core/contexts";
 import { createTshirt } from "testUtils/fixtures";
 
 describe("TShirtPage", () => {
-  test("should display a circular progress while loading data", async () => {
+  test("should display tshirt card with the loaded data", async () => {
     const tshirt = createTshirt();
     render(<TshirtPage />, {
       path: `/tshirts/:tshirtId`,
@@ -14,12 +14,12 @@ describe("TShirtPage", () => {
       wrapper: CartProvider,
     });
 
-    expect(
-      await screen.findByRole("heading", { level: 5 })
-    ).toBeInTheDocument();
+    await waitFor(() =>
+      screen.findByRole("heading", { name: tshirt.name, level: 5 })
+    );
 
     expect(
-      screen.getByRole("heading", { name: tshirt.name, level: 5 })
+      screen.getByRole("button", { name: "Add to cart" })
     ).toBeInTheDocument();
   });
 });
